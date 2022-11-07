57d5c7a2a10d0cbf4826b9c0951e40c387ccbd97
Josh Richmond <jrichmond2009@gmail.com>
2022-11-06 22:42:12 -0500

    deleted hello world

--- hello_world/hello_cargo/target/debug/deps/hello_cargo-84cd33c7f338469a.d
+++
@@ -1,5 +0,0 @@
-/home/richmojo/trading-projects/lets_get_rusty/hello_world/hello_cargo/target/debug/deps/hello_cargo-84cd33c7f338469a: src/main.rs
-
-/home/richmojo/trading-projects/lets_get_rusty/hello_world/hello_cargo/target/debug/deps/hello_cargo-84cd33c7f338469a.d: src/main.rs
-
-src/main.rs:
--- hello_world/hello_cargo/target/debug/hello_cargo.d
+++
@@ -1,1 +0,0 @@
-/home/richmojo/trading-projects/lets_get_rusty/hello_world/hello_cargo/target/debug/hello_cargo: /home/richmojo/trading-projects/lets_get_rusty/hello_world/hello_cargo/src/main.rs
