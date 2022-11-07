d6fed12bb837e62018e7036cad4340c834576703
Josh Richmond <jrichmond2009@gmail.com>
2022-11-06 22:41:59 -0500

    chapter 1

---
+++ ch1/hello_cargo/target/debug/deps/hello_cargo-fec4445fe248af7f.d
@@ -0,0 +1,5 @@
+/home/richmojo/trading-projects/lets_get_rusty/hello_world/hello_cargo/target/debug/deps/hello_cargo-fec4445fe248af7f.rmeta: src/main.rs
+
+/home/richmojo/trading-projects/lets_get_rusty/hello_world/hello_cargo/target/debug/deps/hello_cargo-fec4445fe248af7f.d: src/main.rs
+
+src/main.rs:
