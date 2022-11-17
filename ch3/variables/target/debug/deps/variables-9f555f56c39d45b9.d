25c92fc856e65a87b643b353696a71232873f0e2
Josh Richmond <jrichmond2009@gmail.com>
2022-11-17 12:22:30 -0500

    chapter 3

---
+++ ch3/variables/target/debug/deps/variables-9f555f56c39d45b9.d
@@ -0,0 +1,5 @@
+/home/richmojo/trading-projects/lets_get_rusty/ch3/variables/target/debug/deps/variables-9f555f56c39d45b9: src/main.rs
+
+/home/richmojo/trading-projects/lets_get_rusty/ch3/variables/target/debug/deps/variables-9f555f56c39d45b9.d: src/main.rs
+
+src/main.rs:
---
+++ ch3/variables/target/debug/variables.d
@@ -0,0 +1,1 @@
+/home/richmojo/trading-projects/lets_get_rusty/ch3/variables/target/debug/variables: /home/richmojo/trading-projects/lets_get_rusty/ch3/variables/src/main.rs
